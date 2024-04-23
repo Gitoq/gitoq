93c25471c63a6e9ee4afe7c067d3066e7ab06a49
AmirShafikhani <amirshafikhani.official@gmail.com>
2024-04-23 21:24:49 +0330

    feat(helper): 🔧 auth configuration file functions added

---
+++ src/helper/index.ts
@@ -0,0 +1,28 @@
+import fs from "node:fs";
+import dotenv from "dotenv";
+import { CONFIG_FILE_URL } from "../constants/index.js";
+
+// ? env
+export const configEnv = () => dotenv.config();
+
+// ? config file
+type TJsonFileContent = { token: string };
+
+export const dispatchConfig = (token: string) => {
+  const content: TJsonFileContent = { token };
+  fs.writeFileSync(CONFIG_FILE_URL, JSON.stringify(content), { flag: "a" });
+};
+
+export const getConfig = () => {
+  const content = fs.readFileSync(CONFIG_FILE_URL, { encoding: "utf8" });
+  if (content) {
+    const parsedData = JSON.parse(content) as TJsonFileContent;
+    if (parsedData.token) return parsedData.token;
+  }
+  throw new Error("Please login first");
+};
+
+export const deleteConfig = () => {
+  const isExists = fs.existsSync(CONFIG_FILE_URL);
+  isExists && fs.unlinkSync(CONFIG_FILE_URL);
+};
