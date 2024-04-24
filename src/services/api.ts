c5d9969e91345e1259565fdab9f54f8008747ff2
AmirShafikhani <amirshafikhani.official@gmail.com>
2024-04-24 19:44:24 +0330

    feat(services): 🔧 api instance updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,31 +8,31 @@ const baseURL = process.env.BASE_URL as string;
 type TApi<T> = Promise<{ data: T; response: Response }>;
 
 export const api = async <T>({ data, headers, method, params, url }: TOrvalOptions, options?: TOptions): TApi<T> => {
-  url = baseURL + url;
-  if (params) url += "?" + new URLSearchParams(params);
-
-  const authorization = (options?.headers as any)?.authorization || getConfig();
-
-  const configs: RequestInit = {
-    method,
-    body: JSON.stringify(data),
-    ...(options ?? {}),
-    headers: {
-      ...headers,
-      "Accept-Language": "en",
-      ...(authorization ? { authorization } : {}),
-      ...(options ? options.headers : {}),
-    },
-  };
-
   try {
+    url = baseURL + url;
+    if (params) url += "?" + new URLSearchParams(params);
+
+    const authorization = (options?.headers as any)?.authorization || getConfig();
+
+    const configs: RequestInit = {
+      method,
+      body: JSON.stringify(data),
+      ...(options ?? {}),
+      headers: {
+        ...headers,
+        "Accept-Language": "en",
+        ...(authorization ? { authorization } : {}),
+        ...(options ? options.headers : {}),
+      },
+    };
+
     const response = await fetch(url, configs);
+    const json: T = await response.json();
     if (response.ok) {
-      const data: T = await response.json();
-      return { response, data };
-    } else throw response;
+      return { response, data: json };
+    } else throw json;
   } catch (error: any) {
-    error?.response?.status === 401 && deleteConfig();
+    error?.status === 401 && deleteConfig();
     throw error;
   }
 };
