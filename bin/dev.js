a9bf1d076ea570dd17a03c8c6a0f4646859cec90
Soheil <jafarnejad.official@gmail.com>
2024-04-25 18:24:30 +0330

    fix(bin): 🐛 fixed node/shebang rule eslint

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node_modules/.bin/ts-node
-// eslint-disable-next-line node/shebang, unicorn/prefer-top-level-await
+/* eslint-disable unicorn/prefer-module , unicorn/prefer-top-level-await */
 (async () => {
   const oclif = await import("@oclif/core");
-  await oclif.execute({ development: true, dir: __dirname });
+  await oclif.execute({ dir: __dirname, development: true });
 })();
--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-
-// eslint-disable-next-line unicorn/prefer-top-level-await
+/* eslint-disable unicorn/prefer-module , unicorn/prefer-top-level-await */
 (async () => {
   const oclif = await import("@oclif/core");
-  await oclif.execute({ development: false, dir: __dirname });
+  await oclif.execute({ dir: __dirname, development: false });
 })();
